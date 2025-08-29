9cd4e2b07c6b3d01e942d6fba51eb70d8f97b6a5
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2025-08-29 17:43:38 +0200

    🌲

--- src/features/resource/ProjectHeader.tsx
+++ src/features/resource/ProjectHeader.tsx
@@ -11,9 +11,10 @@ interface Props {
 const ProjectHeader: React.FC<Props> = ({ code, logo, name, uuid }) => <div className='flex gap-4 bg-background p-4 text-foreground light'>
     <img className='max-h-[111px]' src={logo} />
     <div className='flex flex-col justify-end items-start'>
-        <div className='font-mono text-gray-500 text-xs lowercase'>Projet :</div>
-        <div className='font-serif text-2xl'>{name} ffi st ct</div>
-        <Link href={'/id/' + uuid}>{`/p/${code}/`}</Link>
+        <div className='font-serif text-2xl'>{name}</div>
+        <div className='font-mono text-gray-500 text-xs lowercase'>
+            lien vers le projet : <Link className='text-xs' href={'/id/' + uuid}>{`/p/${code}/`}</Link>
+        </div>
     </div>
 </div >
 
