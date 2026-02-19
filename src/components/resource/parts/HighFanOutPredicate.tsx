7c99fb3931474cd5a7a35c596ccef7dcf0059567
BOTTINI Thomas <thomas.bottini@cnrs.fr>
2026-02-19 17:54:01 +0100

    🌲

--- src/components/resource/parts/HighFanOutPredicate.tsx
+++ src/components/resource/parts/HighFanOutPredicate.tsx
@@ -2,12 +2,14 @@ import YasguiButton from "@/components/common/YasguiButton"
 import TableWrapper from "@/components/layout/TableWrapper"
 import { useListLinkedResources } from "@/hooks/sherlockSparql"
 import { ProjectIdData } from "@/utils/project"
-import { Input } from "@heroui/react"
-import { ColumnDef, flexRender, getCoreRowModel, getFilteredRowModel, useReactTable } from '@tanstack/react-table'
+import { Button, Input } from "@heroui/react"
+import { ColumnDef, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, useReactTable } from '@tanstack/react-table'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { SparqlQueryResultObject_Binding } from 'sherlock-rdf/lib/sparql-result'
 import { displayClassOrProperty } from "../BindingTables"
+
+const MIN_N_LINES = 10
 
 export default function HighFanOutPredicate({ n, predicateUri, resourceUri, projectIdData }: {
   n: number
@@ -24,6 +26,8 @@ export default function HighFanOutPredicate({ n, predicateUri, resourceUri, proj
   const { data, query } = useListLinkedResources(resourceUri, predicateUri)
 
   const [globalFilter, setGlobalFilter] = useState("")
+  const [pageIndex, setPageIndex] = useState(0);
+  const [pageSize, setPageSize] = useState(MIN_N_LINES);
 
   const columns: ColumnDef<SparqlQueryResultObject_Binding>[] = [
     {
@@ -45,13 +49,20 @@ export default function HighFanOutPredicate({ n, predicateUri, resourceUri, proj
     data: data?.results.bindings || [],
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
     globalFilterFn: (row, columnId, filterValue) => {
       const search = String(filterValue).toLowerCase()
       return Object.values(row.original).some(value => String(value.value).toLowerCase().includes(search))
     },
     onGlobalFilterChange: setGlobalFilter,
+    onPaginationChange: (updater) => {
+      const newState = typeof updater === "function" ? updater({ pageIndex, pageSize }) : updater;
+      setPageIndex(newState.pageIndex);
+      setPageSize(newState.pageSize);
+    },
     state: {
       globalFilter,
+      pagination: { pageIndex, pageSize },
     }
   })
 
@@ -60,7 +71,7 @@ export default function HighFanOutPredicate({ n, predicateUri, resourceUri, proj
   ////////////////////////////////////////////////////////////////////////////////
 
   return <div className='mt-3 first:mt-0'>
-    <div className='inline-block bg-table-head px-3 py-2 border border-text-text-secondary-foreground border-b-0'>
+    <div className='inline-block bg-table-head px-3 py-2 border border-text-text-secondary-foreground border-b-0 text-sm'>
       <div className='flex items-center gap-2'>
         {displayClassOrProperty(predicateUri)}
       </div>
@@ -79,7 +90,57 @@ export default function HighFanOutPredicate({ n, predicateUri, resourceUri, proj
           />
           <YasguiButton sparqlQuery={query} />
         </div>
-        <table className='mt-3 [&_th,&_td]:p-2 font-serif text-sm'>
+        <div className="my-3">
+          <Button
+            className='rounded-none'
+            onClick={() => table.firstPage()}
+            isDisabled={!table.getCanPreviousPage()}
+          >
+            {'<<'}
+          </Button>
+          <Button
+            onClick={() => table.previousPage()}
+            isDisabled={!table.getCanPreviousPage()}
+            className="disabled:opacity-50 rounded-none"
+          >
+            Précédent
+          </Button>
+          <Button
+            onClick={() => table.nextPage()}
+            isDisabled={!table.getCanNextPage()}
+            className="disabled:opacity-50 rounded-none"
+          >
+            Suivant
+          </Button>
+          <Button
+            className='rounded-none'
+            onClick={() => table.lastPage()}
+            isDisabled={!table.getCanNextPage()}
+          >
+            {'>>'}
+          </Button>
+          <span>
+            Page{" "}
+            <strong>
+              {table.getState().pagination.pageIndex + 1} / {table.getPageCount()}
+            </strong>
+          </span>
+          <select
+            value={table.getState().pagination.pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value));
+              setPageIndex(0); // reset à la première page
+            }}
+            className="p-1 border rounded"
+          >
+            {[MIN_N_LINES, MIN_N_LINES * 2, MIN_N_LINES * 5].map((size) => (
+              <option key={size} value={size}>
+                {size} lignes
+              </option>
+            ))}
+          </select>
+        </div>
+        <table className='[&_th,&_td]:p-2 font-serif text-sm'>
           <thead className="bg-table-head [&_th:nth-child(2)]:text-left">
             {table.getHeaderGroups().map((headerGroup) => (
               <tr key={headerGroup.id}>
